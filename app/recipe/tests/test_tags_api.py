6108f4004bfe4348952b87845dec7e0d24097401
Kaue <kaueoliveira.eav@gmail.com>
2020-05-15 15:32:35 +0700

    Added working list tags endpoint

---
+++ app/recipe/tests/test_tags_api.py
@@ -0,0 +1,67 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Tag
+
+from recipe.serializers import TagSerializer
+
+
+TAGS_URL = reverse('recipe:tag-list')
+
+class PublicTagsApiTests(TestCase):
+     """Tests the publicly available tags API"""
+
+     def setUp(self):
+        self.client = APIClient()
+
+     def test_login_required(self):
+        """Tests if login is required to retrieve tags"""
+
+        res = self.client.get(TAGS_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateTagsApiTests(TestCase):
+    """Tests the authorized user tags API"""
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            email = 'test@example.com',
+            password = 'pass123',
+            name = 'test',
+        )
+
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_tags(self):
+        """Tests retrieving tags    """
+        Tag.objects.create(user=self.user, name='Vegan')
+        Tag.objects.create(user=self.user, name='Dessert')
+
+        res = self.client.get(TAGS_URL)
+
+        tags = Tag.objects.all().order_by('-name')
+        serializer = TagSerializer(tags, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_tags_limited_to_user(self):
+        """Tests that tags returned are for authenticated user"""
+        user2 = get_user_model().objects.create_user(
+                email = 'test2@example.com',
+                password = 'pass1232',
+                name = 'test2',
+        )
+        tag = Tag.objects.create(user=self.user, name='Vegan')
+        Tag.objects.create(user=user2, name='Dessert')
+
+        res = self.client.get(TAGS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], tag.name)
